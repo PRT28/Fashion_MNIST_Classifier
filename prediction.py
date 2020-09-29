e7d954f12b5d24b02fb061b6f66ba17471f5d32b
PRT28 <56737649+PRT28@users.noreply.github.com>
2020-09-29 15:42:27 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 import cv2
 import numpy as np
 
-labels= ['T-shirt', 'trousers', 'pullover', 'dress', 'coat','sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+labels= ['T-shirt', 'trousers', 'pullover', 'dress', 'coat','sandal', 'shirt', 'sneaker', 'bag', 'ankle boot'] #labels for the dataset
 
-model=tf.keras.models.load_model('model.h5')
-path=input()
-img=cv2.imread(path)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img=cv2.resize(img, (28,28))
-img = img.reshape(1, 28, 28, 1)
-img = tf.cast(img, tf.float32)
-preds = model.predict(img)
-label=np.argmax(preds)
-print(labels[label])
+model=tf.keras.models.load_model('model.h5')#loaded the pretrained model saved from 'train_model.py'.
+path=input() #input the name of the image provided in the repo.
+img=cv2.imread(path) #reading the image by opencv
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #the image is read in bgr mode by defaul so converting it to rgb format.
+img=cv2.resize(img, (28,28)) #resizing the image to 28X28
+img = img.reshape(1, 28, 28, 1) #reshaping the image so that it can be fed to neural network.
+img = tf.cast(img, tf.float32) #casting the pixels of the image to float32
+preds = model.predict(img) #pridicting the class of the image.
+label=np.argmax(preds) #calculating the index of the label
+print(labels[label]) #display the class
