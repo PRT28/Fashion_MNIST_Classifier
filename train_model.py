da5ceb204a541909495bf347dec742e719d1be73
PRT28 <56737649+PRT28@users.noreply.github.com>
2020-09-29 15:37:02 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,28 +1,31 @@
 import tensorflow as tf
 
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data() #downloaded the fashion mnist dataset from Keras
 
-labels= ['T-shirt', 'trousers', 'pullover', 'dress', 'coat','sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+labels= ['T-shirt', 'trousers', 'pullover', 'dress', 'coat','sandal', 'shirt', 'sneaker', 'bag', 'ankle boot'] #labels for the dataset
 
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
-test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1) #reshaped the download dataset to 60000 images with each of 28X28 size with 1 channel
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1) ##reshaped the download dataset to 10000 images with each of 28X28 size with 1 channel
 
 model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)), 
         tf.keras.layers.MaxPooling2D(2,2),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dense(10, activation='softmax')
         ])
 
-model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=['accuracy'])
+#optimizer used is adam as optimizes the loss with great measure
+#As we are dealing with multiple classes so sparse_categorical_crossentropy is used
+model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=['accuracy']) 
 
+#trained the model for 40 epochs
 model.fit(
     train_images,train_labels,
     epochs=40,
     verbose=1
     )
-
+#evaluated the models accuracy on test set
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
 print("Test accuracy: ", test_acc)
